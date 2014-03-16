8d24f1789bd498622297cae0ba1a107f4831dc13
Blake Rainwater <rainwaterblake@gmail.com>
2014-03-16 17:06:17 -0400

    Add weather and color to xmobar

--- xmobar/xmobarrc.hs
+++ xmobar/xmobarrc.hs
@@ -1,17 +1,19 @@
 Config { bgColor = "black"
        , fgColor = "grey"
-       , position = TopW L 70
+       , position = Top
        , lowerOnStart = True
-       , commands = [ Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 20
-                    , Run MultiCpu ["-t","Cpu: <autototal>%","-m","2","-L","3","-H","50","--normal","green","--high","red"] 20
-                    , Run Memory ["-t","Mem: <usedratio>%","-L","50","-H","80","--normal","green","--high","red"] 60
-                    , Run Swap [] 600
-					, Run DiskU [("/", "<used>/<size>"), ("sdb1", "<usedbar>")] ["-L", "20", "-H", "50", "-m", "1", "-p", "3"] 20
-                    , Run Battery ["-L","20","-H","80","--low","red","--normal","green"] 20
-                    , Run Date "%a %b %_d %k:%M" "date" 20
-                    , Run StdinReader
-                    ]
+       , commands = [ Run Weather "KPDK" ["-t","<station>: <tempF>°F, <skyCondition>, <rh>% chance of rain","-S","True","-p","2","-L","45","-H","75","--normal","green","--high","red","--low","lightblue"] 600
+                      , Run Cpu ["-t","<fc=LightBlue>Cpu</fc>: <total>","-S","True","-p","2","-L","10","-H","50","--normal","green","--high","red"] 20
+                      , Run MultiCpu ["-t","<fc=LightBlue>Cpu</fc>: <autototal>","-p","2","-S","True","-m","2","-L","10","-H","50","--normal","green","--high","red"] 20
+                      , Run Memory ["-t","<fc=LightBlue>Mem</fc>: <usedratio>%","-L","50","-H","80","--normal","green","--high","red"] 60
+                      , Run Swap ["-t","<fc=LightBlue>Swap</fc>: <usedratio>","-p","2","-S","True"] 600
+                      , Run DiskU [("/", "<fc=LightBlue>sdb1</fc>: <used>/<size>"), ("sdb1", "<usedbar>")] ["-L", "20", "-H", "50", "-m", "1", "-p", "3"] 20
+                      , Run Battery ["-L","20","-H","80","--low","red","--normal","green"] 20
+                      , Run Date "%a %b %_d %k:%M" "date" 20
+                      , Run StdinReader
+                      ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%StdinReader% }{ %cpu% | %multicpu% | %memory% * %swap% | %disku% | %battery% | <fc=#ee9a00>%date%</fc> "
+       , template = "%StdinReader% }{ %KPDK% | %cpu% | %multicpu% | %memory% * %swap% | %disku% | %battery% | <fc=#ee9a00>%date%</fc> "
        }
+  
