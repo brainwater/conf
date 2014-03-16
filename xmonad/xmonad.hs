dddb220e8141ebd087148f59d154b49e38a32aab
Blake Rainwater <rainwaterblake@gmail.com>
2014-03-16 17:30:33 -0400

    Fix locking screen with W-z

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -9,6 +9,7 @@ import System.IO
 main = do
   xmproc <- spawnPipe "xmobar"
   spawn "bash ${HOME}/.xmonad/autostart.bash"
+  spawn "xscreensaver -no-splash"
   xmonad $ defaultConfig
      { manageHook = manageDocks <+> manageHook defaultConfig
      , layoutHook = avoidStruts $ smartBorders $  layoutHook defaultConfig
@@ -18,5 +19,5 @@ main = do
                  }
      , modMask = mod4Mask     -- Rebind Mod (default Alt) to Windows key
      } `additionalKeys`
-     [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+     [ ((mod4Mask, xK_z), spawn "xscreensaver-command -lock")
      ]
